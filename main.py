c6552ebd1fa5622cf5bf8958325d5b20d1ec42c3
Md Zaid Quaraishi <mzq7080@gmail.com>
2025-06-11 00:27:01 +0530

    complete mouse without mouse app

---
+++ main.py
@@ -0,0 +1,46 @@
+import keyboard
+import mouse
+
+COMBINATION = "ctrl+alt+windows"
+COORDINATE_BASE_VAL = 10
+SCROLL_BASE_VAL = 2
+
+def moveMouse(x, y):
+    mouse.move(y, x, False)
+
+def registerHotKeys(combination, func):
+    keyboard.add_hotkey(COMBINATION + "+" + combination, lambda: func(), suppress=True)
+
+print("=========================================")
+print("mouse without mouse program started...")
+print("=========================================")
+
+
+# simple motions
+registerHotKeys("up", lambda: moveMouse(COORDINATE_BASE_VAL*-1, 0))
+registerHotKeys("down", lambda: moveMouse(COORDINATE_BASE_VAL, 0))
+registerHotKeys("left", lambda: moveMouse(0, COORDINATE_BASE_VAL*-1))
+registerHotKeys("right", lambda: moveMouse(0, COORDINATE_BASE_VAL))
+
+# diagonal movements
+registerHotKeys("up+right", lambda: moveMouse(COORDINATE_BASE_VAL*-1, COORDINATE_BASE_VAL))
+registerHotKeys("up+left", lambda: moveMouse(COORDINATE_BASE_VAL*-1, COORDINATE_BASE_VAL*-1))
+
+registerHotKeys("down+right", lambda: moveMouse(COORDINATE_BASE_VAL, COORDINATE_BASE_VAL))
+registerHotKeys("down+left", lambda: moveMouse(COORDINATE_BASE_VAL, COORDINATE_BASE_VAL*-1))
+
+registerHotKeys("left+up", lambda: moveMouse(COORDINATE_BASE_VAL*-1, COORDINATE_BASE_VAL*-1))
+registerHotKeys("left+down", lambda: moveMouse(COORDINATE_BASE_VAL, COORDINATE_BASE_VAL*-1))
+
+registerHotKeys("right+up", lambda: moveMouse(COORDINATE_BASE_VAL*-1, COORDINATE_BASE_VAL))
+registerHotKeys("right+down", lambda: moveMouse(COORDINATE_BASE_VAL, COORDINATE_BASE_VAL))
+
+# keyboard mappings with mouse actions
+registerHotKeys("del", lambda: mouse.click())
+registerHotKeys("end", lambda: mouse.right_click())
+registerHotKeys("insert", lambda: mouse.double_click())
+
+registerHotKeys("page up", lambda: mouse.wheel(SCROLL_BASE_VAL))
+registerHotKeys("page down", lambda: mouse.wheel(-SCROLL_BASE_VAL))
+
+keyboard.wait()
---
+++ readme.md
@@ -0,0 +1,28 @@
+## Description
+
+When this script is running
+we can use a combination of keys plus up, down, left and right to move mouse pointer and take actions like left click, right click, double click, scroll up and scroll down
+
+default combinations are-
+
+-   ctrl+alt+windows (base_combination)
+-   base_combination + (up, down, right, left) - for movement
+-   base_combination + del - left click
+-   base_combination + end - right click
+-   base_combination + insert - double click
+-   base_combination + page up - scroll up
+-   base_combination + page down - scroll down
+
+Note:- can be changed in code
+
+## Installation
+
+-   pip install keyboard
+-   pip install mouse
+-   run main.py
+
+## Limitations
+
+-   currently supported for windows only
+
+PS- This repo is open for additions/improvements through PRs
